6299d84fde291d7fa7d9904c1ad805cda700d73d
sunyirong <995334152@qq.com>
2018-12-05 12:24:56 +0800

    完成13章

--- settings.py
+++ settings.py
@@ -6,11 +6,21 @@ class Settings():
 		self.screen_width = 1000
 		self.screen_height = 600
 		self.bg_color = (230,230,230)
-		self.ship_speed_factor = 10
-
+	
 		# 子弹设置
 		self.bullet_speed_factor = 5
-		self.bullet_width = 3
+		self.bullet_width = 100
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
-		self.bullets_allowed = 3
+		self.bullets_allowed = 10
+
+		# 外星人设置
+		self.alien_speed_factor = 10
+		self.fleet_drop_speed = 100
+		# 1表示右移，-1表示左移
+		self.fleet_direction = 1
+
+		# 飞船设置
+		self.ship_speed_factor = 10
+		self.ship_limit = 2
+
